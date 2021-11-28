27575967d7df92c3c2bef208bf22576089daa479
chandranitu <chandranitu@gmail.com>
2021-11-28 08:09:17 +0530

    successfull CI-CD

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello chandra12345!');
+  res.write('Hello Welcome to CI -CD pipeline!');
   res.end();
 }).listen(8080, '0.0.0.0');
